c8cb2a65a155c6b2df3699c2f88cd89ee361a82f
Lucas Martins <lucasmartins@MacBook-Pro-de-Lucas.local>
2022-02-17 00:16:38 -0300

    Tempo de execucao com %timeit

---
+++ pythonic-code_tempo_execucao_timeit.py
@@ -0,0 +1,12 @@
+# Examinando o tempo de execucao com %timeit
+
+## compreensão de lista versus descompactar
+
+## Rodar no terminal:
+
+# Comppressao de lista
+#%timeit [num for num in range(51)]
+
+# Desempacotar
+#%timeit [*range(51)]
+
